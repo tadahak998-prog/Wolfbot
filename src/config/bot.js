3d09a3174016ab5959bb96be967bb990079515fe
tadahak998-prog <tadahak998@gmail.com>
2026-05-09 02:29:00 +0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "By Amane ",
+        name: "Amane ",
         // Activity type number (0 = Playing).
         type: 0, 
       },
